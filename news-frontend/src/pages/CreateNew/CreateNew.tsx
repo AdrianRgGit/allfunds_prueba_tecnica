2fe57cf12d78c8806e12ea5c5ce745265da57dce
Adrián Ramírez <adrianramirezgalera@gmail.com>
2025-01-27 10:58:20 +0100

    dejamos el front por el momento terminado y comenzamos back

--- news-frontend/src/pages/CreateNew/CreateNew.tsx
+++ news-frontend/src/pages/CreateNew/CreateNew.tsx
@@ -3,7 +3,7 @@ import NewsForm from "../../components/News/NewsForm/NewsForm";
 const CreateNew = () => {
   return (
     <main className="mx-auto w-[80%]">
-      <h2 className="text-center text-2xl font-bold">Crear Noticia</h2>
+      <h2 className="text-center text-2xl font-bold mb-4">Crear Noticia</h2>
       <NewsForm />
     </main>
   );
