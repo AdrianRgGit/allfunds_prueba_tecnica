cdc1653449b27036376bb1f215c82eeafbcdd6c6
Adrián Ramírez <adrianramirezgalera@gmail.com>
2025-01-27 11:47:31 +0100

    creado seeder para news

--- news-backend/src/models/News.js
+++ news-backend/src/models/News.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
-const newsSchema = new mongoose.Schema({
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { type: String, required: true },
-  archiveDate: { type: Date, default: null },
-});
+const newsSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: String, required: true },
+    archiveDate: { type: Date, default: null },
+  },
+  { timestamps: true }
+);
 
 const News = mongoose.model("News", newsSchema);
 
